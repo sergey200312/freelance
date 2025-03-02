99b72421cb8e7858eb488a897f3ba460ef5a26c3
sergey200312 <serezha.mikhaylov.03@list.ru>
2025-03-02 21:45:47 +0500

    создал сущность для user

---
+++ server/src/user/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import { UserRole } from "../roles.enum";
+
+export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+
+    @Prop({ type: String, unique: true, required: true })
+    email: string
+
+    @Prop({ type: String, required: true })
+    password: string
+    
+    @Prop({ type: String, required: true })
+    firstName: string
+
+    @Prop({ type: String, required: true })
+    lastName: string
+
+    @Prop({ required: true, enum: Object.values(UserRole) })
+    role: UserRole
+
+    @Prop({ type: String })
+    avatar_url?: string
+
+}
