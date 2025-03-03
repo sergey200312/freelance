0abd689bf365a664ed4f316f1343e6aa123ecea8
sergey200312 <serezha.mikhaylov.03@list.ru>
2025-03-03 18:51:50 +0500

    #5.1 добавил конструктор и поменял название

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,20 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.userService.register(createUserDto);
   }
 
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,13 +1,18 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import * as argon2 from 'argon2'
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  userModel: any;
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+  
   async register(createUserDto: CreateUserDto) {
-    
+
     const existUser = await this.userModel.findOne({ email: createUserDto.email });
 
     if (existUser) {
